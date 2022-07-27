3fac6cbf24f5d4bb74c9ad7b2a10dfe58ff59ffd
fezaden <68229872+fezaden@users.noreply.github.com>
2022-07-27 22:43:49 +0300

    Flask First Commit

---
+++ todo.py
@@ -0,0 +1,54 @@
+from turtle import title
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Bilge/Desktop/TODOAPP/todo.db'
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def index():
+    todos = Todo.query.all()
+    return render_template("index.html", todos=todos)
+
+
+@app.route("/copmlete/<string:id>")
+def copmleteTodo(id):
+    todo = Todo.query.filter_by(id=id).first()
+    # if todo.copmlete == True:
+    #     todo.copmlete == False
+    # else:
+    #     todo.copmlete = True
+    todo.copmlete = not todo.copmlete
+
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/add", methods=["POST"])
+def addTodo():
+    title = request.form.get("title")
+    newTodo = Todo(title=title, copmlete=False)
+    db.session.add(newTodo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/delete/<string:id>")
+def deleteTodo(id):
+    todo = Todo.query.filter_by(id=id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    copmlete = db.Column(db.Boolean)
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
